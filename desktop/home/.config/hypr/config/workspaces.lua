ad492564b17309e3e964df0e0b67fa0ac56f393e
dothoard <dothoard@localhost>
2026-08-22 12:46:28 -0300

    backup(cachyos-x8664): 2026-08-22 15:46:27

--- desktop/home/.config/hypr/config/workspaces.lua
+++ desktop/home/.config/hypr/config/workspaces.lua
@@ -1,6 +1,6 @@
 -- Workspace rules wiki https://wiki.hypr.land/Configuring/Basics/Workspace-Rules/
 -- Add your workspace rules here. Increment the workspace number as you go. Do not have duplicate workspaces.
-hl.workspace_rule({ workspace = "name:gaming", monitor = PRIMARY_MONITOR, default = true })
+hl.workspace_rule({ workspace = "name:gaming", monitor = PRIMARY_MONITOR })
 
 -- Main monitor (DP-3): workspaces 1-10
 hl.workspace_rule({ workspace = "1", monitor = MONITOR1, default = true, persistent = true })
